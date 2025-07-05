eeb21eb7fdd41e003ec0cd34a72ad4b90be1c4e4
Patrik Jonsson <code@familjenjonsson.org>
2025-07-05 11:54:04 +0200

    Removed changeover and quality sensor type.

--- custom_components/heru/const.py
+++ custom_components/heru/const.py
@@ -397,19 +397,6 @@ HERU_SENSORS = [
         "register_type": DISCRETE_INPUTS,
     },
     {
-        "name": "Quality sensor 1 type",
-        "modbus_address": "3x00041",
-        "address": 40,
-        "scale": None,
-        "icon": ICON_CALENDAR,
-        "unit_of_measurement": None,
-        "device_class": SensorDeviceClass.ENUM,
-        "state_class": None,
-        "entity_category": None,
-        "register_type": INPUT_REGISTERS,
-        "options": ["None", "RH", "CO2", "VOC"],
-    },
-    {
         "name": "Quality sensor 1 value",
         "modbus_address": "3x00042",
         "address": 41,
@@ -421,19 +408,6 @@ HERU_SENSORS = [
         "entity_category": None,
         "register_type": INPUT_REGISTERS,
 	},
-	{
-        "name": "Changeover active",
-        "modbus_address": "3x00034",
-        "address": 33,
-        "scale": None,
-        "icon": ICON_CALENDAR,
-        "unit_of_measurement": None,
-        "device_class": SensorDeviceClass.ENUM,
-        "state_class": None,
-        "entity_category": None,
-        "register_type": INPUT_REGISTERS,
-        "options": ["Off", "On"],
-    },
 ]
 
 
